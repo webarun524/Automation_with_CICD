9a1c863f718851a56c152807f6867b13a6a639fb
Mass Arun <massarun@Masss-MacBook-Pro.local>
2026-02-12 15:04:08 +0530

    removed network issue

--- tests/test_add_user.py
+++ tests/test_add_user.py
@@ -6,7 +6,5 @@ def test_add_user(page):
 
     login_page.open_login_page()
     login_page.login("Admin", "admin123")
-    page.wait_for_load_state("networkidle")
 
     login_page.add_user()
-    page.wait_for_load_state("networkidle")
